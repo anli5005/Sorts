e8daa0aee0bae38b556ad2ee2427af8d08a21710
Anthony <li.anthony917@gmail.com>
2014-04-24 15:49:06 -0400

    Improved API

--- apis/Sorts.js
+++ apis/Sorts.js
@@ -59,6 +59,10 @@ Sorts.Log.logError = function (error) {
 	}
 }
 
+Sorts.getSortName = function (num) {
+	return "sort_"+num.toString();
+}
+
 Sorts.getAllSorts = function () {
 	return Sorts.JSON.parse(localStorage.getItem("sort_all"));
 }
@@ -66,4 +70,91 @@ Sorts.getAllSorts = function () {
 Sorts.SortMeta = function (name) {
 	this.name = name;
 	return this;
+}
+Sorts.SortMeta.metaNameForSort = function (name) {
+	return name+"_meta";
+}
+Sorts.SortMeta.metaForSort = function (num) {
+	var metaJson = localStorage.getItem(Sorts.SortMeta.metaNameForSort(Sorts.getSortName(num)));
+	if (metaJson) {
+		var parsedMeta = Sorts.JSON.parse(metaJson);
+		if (parsedMeta.parseData) {
+			return new Sorts.SortMeta(parsedMeta.parseData["name"]);
+		} else {
+			Sorts.Log.logError(parsedMeta.error);
+		}
+	}
+}
+
+Sorts.SortTerm = function (termName) {
+	this.text = termName;
+	this.isBold = false;
+	return this;
+}
+Sorts.SortTerm.termFromData = function (termData) {
+	var sortTerm = new Sorts.SortTerm(termData["text"]);
+	sortTerm.isBold = termData["isBold"];
+	return sortTerm;
+}
+
+Sorts.SortColumn = function (columnName) {
+	this.name = columnName;
+	this.terms = [];
+	this.addTerm = function (term) {
+		this.terms.push(term);
+	}
+	return this;
+}
+Sorts.SortColumn.columnFromData = function (columnData) {
+	var sortColumn = new Sorts.SortColumn(columnData["name"]);
+	var sortData = columnData["terms"];
+	if (sortData) {
+		var termNum = 0;
+		while (termNum < sortData.length) {
+			sortColumn.addTerm(Sorts.SortTerm.termFromData(sortData[termNum]));
+			termNum = termNum + 1;
+		}
+	}
+	return sortColumn;
+}
+
+Sorts.Sort = function () {
+	this.meta = new Sorts.SortMeta();
+	this.sortNumber = null;
+	this.sortColumns = [];
+	return this;
+}
+Sorts.Sort.getSort = function (num) {
+	var sort = new Sorts.Sort();
+	sort.sortNumber = num;
+	sort.meta = Sorts.SortMeta.metaForSort(num);
+	var json = localStorage.getItem(Sorts.getSortName(num));
+	if (json) {
+		var parsed = Sorts.JSON.parse(json);
+		if (parsed.parseData) {
+			var i = 0;
+			while (i < parsed.parseData.length) {
+				sort.sortColumns.push(Sorts.SortColumn.columnFromData(parsed.parseData[i]));
+				i = i + 1;
+			}
+		} else {
+			if (parsed.error) {
+				Sorts.Log.logError(parsed.error);
+			}
+		}
+	}
+	return sort;
+}
+
+Sorts.removeSort = function (num) {
+	var current = Sorts.getAllSorts();
+	if (current.error) {
+		return current.error;
+	}
+	var removed;
+	if (current.parseData) {
+		if (current.parseData.indexOf(Sorts.getSortName(num)) == -1) {
+			return new Error("There is no such sort as sort "+num);
+		}
+	}
 }
